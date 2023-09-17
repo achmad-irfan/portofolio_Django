5c7766abf5dbe608ae8bb6c653fa20feb67f0228
achmad-irfan <achmad.irfan1@yahoo.com>
2023-09-17 23:36:14 +0700

    edit setting

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT= '/home/public_html/portfolio2'
+STATIC_ROOT= '/home/achmadi1/public_html/portfolio2'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 
